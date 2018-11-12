841cb241d7c4e834df50f0146affacc2279e1c7b
masterUng <ubc1515@hotmail.com>
2018-11-12 09:50:42 +0700

    Initial commit

---
+++ app/src/main/java/helper/UrlApp.java
@@ -0,0 +1,52 @@
+package helper;
+
+public class UrlApp {
+
+    public static final String API4 = "http://uwaybear.homeip.net/api/";
+    public static final String API2 = "http://2654k.com/api/";
+    public static final String API3 = "http://pataya.bear-tv.com/api/";
+    public static final String API5 = "http://uway.bear-tv.com/api/";
+    public static final String API = "http://2654k.com/api/";
+
+
+    //token
+    public static final String CHECK_TOKEN = API5 + "token_check";
+    //user
+    public static final String REGISTER = API2 + "register";
+    public static final String LOGIN = API2 + "login";
+    public static final String CHANGEPASS = API2 + "change_password";
+    public static final String BANK_ACCOUNT = API2 + "bankaccount";
+    public static final String TRANSFER = API2 + "reqtransfer";
+    public static final String CHECK_EXPIRED = API2 + "check_expired/";
+    public static final String CHECK_MONEY = API2 + "check_truemoney/";
+    //movie
+    public static final String MAIN_MOVIE = API + "main_category";
+    public static final String CATEGORY_MOVIE = API + "category";
+    public static final String LAST_MOVIE = API4 + "getlastmovie";
+    //series
+    public static final String MAIN_Series = API4 + "series_main_category";
+    public static final String CATEGORY_Series = API4 + "series_sub_category";
+    public static final String SEASON_Series = API4 + "series_season";
+    public static final String Series = API4 + "series";
+    //tv
+    public static final String MAIN_TV = API + "iptv_main_category";
+    public static final String CATEGORY_TV = API + "iptv_sub_category";
+    //search
+    public static final String SEARCH = API4 + "findmovie";
+    //search
+    public static final String SEARCHSR = API4 + "findseries";
+    //other
+    public static final String OTHER = API2 + "other";
+    //slide
+    public static final String SLIDE = API2 + "slide";
+    //ebook
+    public static final String EBOOK_CATEGORY = API + "ebook_category";
+    public static final String EBOOK_ID = API + "ebook";
+    //ip
+    public static final String GET_IP = API2 + "getip";
+    public static final String OTP = API3 + "otp";
+    //paypal
+    public static final String PAY_PAL = API2 + "paypal?id=";
+    public static final String KEEP_ALIVE = API2 + "keep_alive";
+
+}
